4a5c01ad71205c32e5e0c804b7ce75b4279a0c56
koksharov1998 <koksharov1998@ya.ru>
2019-05-14 22:09:46 +0500

    Works without cache

--- dns.py
+++ dns.py
@@ -1,4 +1,4 @@
-import json
+import pickle
 import socket
 
 import dnslib
@@ -20,15 +20,15 @@ forwarder = FORWARDER
 
 
 def save_cache(data):
-    file = open('cache.json', 'w')
-    json.dump(data, file)
+    file = open('cache', 'w')
+    pickle.dump(data, file)
     file.close()
 
 
 def load_cache():
     try:
-        file = open('cache.json', 'r')
-        new_cache = json.load(file)
+        file = open('cache', 'r')
+        new_cache = pickle.load(file)
         file.close()
         return new_cache
     except:
@@ -38,6 +38,7 @@ def load_cache():
 cache = load_cache()
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(('127.0.0.1', 53))
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
 def parse_data(data):
@@ -60,26 +61,38 @@ def parse_data(data):
 while True:
     request, address = server.recvfrom(2048)
     x = dnslib.DNSRecord.parse(request)
-    '''
-    if x.header.qr:
-        print(x.questions)
+
+    if cache.get((x.questions[0].qname, x.questions[0].qtype)):
+        print('cache!')
+        header = dnslib.DNSHeader(x.header.id, q=1, a=len(cache.get((x.questions[0].qname, x.questions[0].qtype))))
+        response = dnslib.DNSRecord(header, x.questions, cache.get((x.questions[0].qname, x.questions[0].qtype)))
+        server.sendto(response.pack(), address)
     else:
-        print(x.questions)
+        client.sendto(request, (forwarder, 53))
+        response_from_dns, _ = client.recvfrom(2048)
+        y = dnslib.DNSRecord.parse(response_from_dns)
+        cache[(y.questions[0].qname, y.questions[0].qtype)] = y.rr
+        # save_cache(cache)
+        header = dnslib.DNSHeader(x.header.id, q=1, a=len(cache.get((x.questions[0].qname, x.questions[0].qtype))))
+        response = dnslib.DNSRecord(header, x.questions, cache.get((x.questions[0].qname, x.questions[0].qtype)))
+        server.sendto(response.pack(), address)
+
     '''
-    # dnslib.QR
     print(x)
-    print(x.questions)
+    print(x.questions[0])
     print(x.ar)
     print()
-
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client.sendto(request, (forwarder, 53))
-    response, addr2 = client.recvfrom(2048)
-    y = dnslib.DNSRecord.parse(response)
+    '''
+    #client.sendto(request, (forwarder, 53))
+    #response, addr2 = client.recvfrom(2048)
+    #y = dnslib.DNSRecord.parse(response)
+    '''
     print(y)
     print(y.questions)
     print(y.ar)
     print(y.rr)
     print(y.auth)
     print()
-    server.sendto(response, address)
+    '''
+    #server.sendto(response, address)
+    #save_cache(cache)
