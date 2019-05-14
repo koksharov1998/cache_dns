a9522ff2854e3b15184f6ad5726c412b8f7835ba
koksharov1998 <koksharov1998@ya.ru>
2019-05-14 23:52:52 +0500

    Can get the Authority section

--- dns.py
+++ dns.py
@@ -1,5 +1,6 @@
 import pickle
 import socket
+import time
 
 import dnslib
 
@@ -30,7 +31,6 @@ def load_cache():
         file = open('cache', 'rb')
         new_cache = pickle.load(file)
         file.close()
-        print(new_cache)
         return new_cache
     except:
         return {}
@@ -44,20 +44,30 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while True:
     request, address = server.recvfrom(2048)
     x = dnslib.DNSRecord.parse(request)
-    print(x)
+    # print(x)
 
     if cache.get((x.questions[0].qname, x.questions[0].qtype)):
         print('cache!')
-        header = dnslib.DNSHeader(x.header.id, q=1, a=len(cache.get((x.questions[0].qname, x.questions[0].qtype))))
-        response = dnslib.DNSRecord(header, x.questions, cache.get((x.questions[0].qname, x.questions[0].qtype)))
+        header = dnslib.DNSHeader(x.header.id, q=1, a=len(cache.get((x.questions[0].qname, x.questions[0].qtype))[0]))
+        response = dnslib.DNSRecord(header, x.questions, cache.get((x.questions[0].qname, x.questions[0].qtype))[0])
         server.sendto(response.pack(), address)
     else:
-        client.sendto(request, (forwarder, 53))
-        response_from_dns, _ = client.recvfrom(2048)
-        y = dnslib.DNSRecord.parse(response_from_dns)
-        print(y)
-        cache[(y.questions[0].qname, y.questions[0].qtype)] = y.rr
-        save_cache(cache)
-        header = dnslib.DNSHeader(x.header.id, q=1, a=len(cache.get((x.questions[0].qname, x.questions[0].qtype))))
-        response = dnslib.DNSRecord(header, x.questions, cache.get((x.questions[0].qname, x.questions[0].qtype)))
-        server.sendto(response.pack(), address)
+        print(x)
+        try:
+            client.sendto(request, (forwarder, 53))
+            response_from_dns, _ = client.recvfrom(2048)
+            y = dnslib.DNSRecord.parse(response_from_dns)
+            print(y)
+            cache[(y.questions[0].qname, y.questions[0].qtype)] = y.rr, time.time()
+            print()
+            print(y.auth)
+            print()
+            if y.auth:
+                cache[(y.auth[0].rname, y.auth[0].rtype)] = y.auth, time.time()
+            save_cache(cache)
+            header = dnslib.DNSHeader(x.header.id, q=1,
+                                      a=len(cache.get((x.questions[0].qname, x.questions[0].qtype))[0]))
+            response = dnslib.DNSRecord(header, x.questions, cache.get((x.questions[0].qname, x.questions[0].qtype))[0])
+            server.sendto(response.pack(), address)
+        except Exception as e:
+            print(e)
